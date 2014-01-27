9c3a7a8628eff6ae7913bc8f384fca0d0497b464
gilmae <avocadia@gmail.com>
2014-01-27 20:14:33 +1100

    Chaos!
    Success checks for challenges now use “dice rolls”

--- lib/adventure_capital/challenge.rb
+++ lib/adventure_capital/challenge.rb
@@ -5,10 +5,17 @@ module AdventureCapital
 		def encounter party
        success = true
 
-       @targets.each{|k,v| success &= party.ability(k) > v}
+       @targets.each{|k,v| success &= roll(party.ability(k)) > v}
 
        party.take_damage(@damage) unless success
        success
+		end
+
+		def roll dice
+			r = Random.new
+			total = 0
+			dice.times { total += r.rand(1..6)}
+			total
 		end
 	end
 end
